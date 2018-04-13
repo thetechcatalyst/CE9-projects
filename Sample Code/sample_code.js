97d814e09b90d89d3ec425f987e23a36dda94a50
technologyordie <adam@fullspansolutions.com>
2018-04-13 15:19:49 -0400

    Added Sample Code

---
+++ Sample Code/sample_code.js
@@ -0,0 +1,28 @@
+/**
+* Sample Code written by Adam Schaeffer
+* http://technologyordie.com
+*/
+const xapi = require('xapi');
+
+//print out the full content of the event object
+function test1(event) {
+  console.log(event);
+}
+
+//print panel id to log file
+function test2(event) {
+  console.log("The button pressed was " + event.PanelId);
+}
+
+//put an on screen popup for 10 seconds with panel id
+//Equal to: xcommand UserInterface Message Alert Display Duration: 10 Text: "The event id is: panel1" Title: "eventid"
+function test3(event) {
+  xapi.command('UserInterface Message Alert Display', {
+    Title: 'Event ID',
+    Text: 'The event ID is: ' + event.PanelId,
+    Duration: 10,
+  });
+}
+
+//Listen for event to happen.  Change callback function to experiment
+xapi.event.on('UserInterface Extensions Panel Clicked', test1);
